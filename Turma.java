7ccdc9a5ade55777d48bf44809bc4efff34cefb1
Ana Maria <119522800+AnaMariaLiborio@users.noreply.github.com>
2025-11-30 22:17:47 -0300

    Create Turma.java

---
+++ Turma.java
@@ -0,0 +1,20 @@
+package com.escola_api.escola_api.model;
+
+public class Turma {
+
+    private Long id;
+    private String nome;
+
+    public Turma() {}
+
+    public Turma(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
